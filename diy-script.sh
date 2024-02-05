1b9b1b20ea0eb325bdc16a1ada2f9ceddddae084
stardust-1 <36785148+stardust-1@users.noreply.github.com>
2024-02-05 18:26:47 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -43,9 +43,9 @@ git clone --depth=1 https://github.com/brektrou/rtl8821CU package/kernel/rtl8821
 
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Themes
